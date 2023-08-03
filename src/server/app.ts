822f519795e83556471b7b9311a5ca7eea98049b
Haydar <haydaramrurevanda@mail.ugm.ac.id>
2023-08-03 19:46:03 +0700

    modified until it works

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,1 +1,19 @@
 import express from "express";
+import { db } from "../config/db.config";
+import { router } from "../routes/post.routes";
+
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.use("/express-typescript/v1/testimonial", router);
+
+// db connection then server connection
+db.then(() => {
+    app.listen(7070, () => {
+        console.log("Server is running on port 7070");
+    })
+});
