189f58f22c7edfdada630ecd27d2aebf41157e14
Haydar <haydaramrurevanda@mail.ugm.ac.id>
2023-08-15 22:39:09 +0700

    modified connect messages

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -9,7 +9,7 @@ const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 const dbName = process.env.DB_NAME;
 
-// connection string to mongo atlas
+// connection string to mongoDB atlas
 const connectionString = `mongodb+srv://${username}:${password}@cluster0.85fxdxq.mongodb.net/${dbName}`;
 
 const options = {
@@ -18,14 +18,14 @@ const options = {
     maxPoolSize: 10,            // Maintain up to 10 socket connections
     serverSelectionTimeoutMS: 5000,     // Keep trying to send operations for 5 seconds
     socketTimeoutMS: 45000,     // Close sockets after 45 seconds of inactivity
-    family: 4                   // Use IPv$, skip trying IPv6
+    family: 4                   // Use IPv4, skip trying IPv6
 };
 
 // db connection
 export const db = mongoose.connect(connectionString, options)
     .then(res => {
         if(res) {
-            console.log(`Database connection successfully to ${dbName}`);
+            console.log(`Successfully Connecting Database to ${dbName}`);
         }
     })
     .catch(err => {
